a02de8ccea308de53597ca02ecc6e5e56066f6b0
Stormuke <fen1x@yandex.ru>
2021-08-12 01:00:06 +0300

    fix: popup

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let popup = document.querySelector('.popup');
 let editBtn = document.querySelector('.profile__edit-button');
+let closeBtn = document.querySelector('.popup__button-close');
 function openPopup() {
   popup.classList.add('popup_opened');
 }
 editBtn.addEventListener('click', openPopup);
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+closeBtn.addEventListener('click', closePopup);
