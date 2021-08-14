8945db7c6a3572405884993cb3d2e401ac682fe8
Stormuke <fen1x@yandex.ru>
2021-08-14 20:39:17 +0300

    add: readme.md, fix: adaptive, fix: submit form

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,15 @@ let popup = document.querySelector(".popup");
 let editBtn = document.querySelector(".profile__edit-button");
 let closeBtn = document.querySelector(".popup__button-close");
 let submitBtn = document.querySelector(".popup__submit");
-let form = document.querySelector('.popup__container');
+let form = document.querySelector(".popup__container");
 
 function popupOpen() {
   popup.classList.add("popup_opened");
 
-  let profileTitleContent = document.querySelector('.profile__title').textContent;
-  let profileSubtitleContent = document.querySelector('.profile__subtitle').textContent;
+  const profileTitleContent =
+    document.querySelector(".profile__title").textContent;
+  const profileSubtitleContent =
+    document.querySelector(".profile__subtitle").textContent;
   document.form["popup-title"].value = profileTitleContent;
   document.form["popup-subtitle"].value = profileSubtitleContent;
 }
@@ -20,7 +22,7 @@ editBtn.addEventListener("click", popupOpen);
 closeBtn.addEventListener("click", popupClose);
 
 document.addEventListener("keydown", function (event) {
-  let key = event.key;
+  const key = event.key;
   if (key === "Escape") {
     popupClose();
   }
@@ -28,9 +30,10 @@ document.addEventListener("keydown", function (event) {
 
 function popupSubmit(evt) {
   evt.preventDefault();
-  let popupTitle = document.form["popup-title"].value;
-  let popupSubtitle = document.form["popup-subtitle"].value;
-  document.querySelector('.profile__title').textContent = popupTitle;
-  document.querySelector('.profile__subtitle').textContent = popupSubtitle;
+  const popupTitle = document.form["popup-title"].value;
+  const popupSubtitle = document.form["popup-subtitle"].value;
+  document.querySelector(".profile__title").textContent = popupTitle;
+  document.querySelector(".profile__subtitle").textContent = popupSubtitle;
+  popupClose();
 }
-form.addEventListener('submit', popupSubmit);
+form.addEventListener("submit", popupSubmit);
