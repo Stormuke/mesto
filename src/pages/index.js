06b0b3fbd08fe53e34de431a96f449e1387d3b2b
Stormuke <fen1x@yandex.ru>
2021-10-11 20:41:56 +0300

    style: fix style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import './index.css'
+import '../pages/index.css'
 
 import {
   addBtn,
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader',
+          options: {importLoaders: 1}
+        },
+          'postcss-loader']
+      },
+      {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: /node_modules/
@@ -29,23 +37,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, {
-          loader: "css-loader",
-          options: {importLoaders: 1}
-        },
-          'postcss-loader']
       }
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({filename: 'pages/[name].css'}),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       inject: "body"
     }),
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin()
+    new CleanWebpackPlugin()
   ]
 }
+
